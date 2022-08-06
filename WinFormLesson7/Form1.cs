1ac409f754d68fdba0a031ab9896667f7c7cbb09
Sule_jr92 <Sule_jr92@STHQ0129-05>
2022-08-06 11:06:21 +0400

    Add project files.

---
+++ WinFormLesson7/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormLesson7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            foreach (var item in this.Controls)
+            {
+                if(item is Label lb)
+                {
+                    lb.Font = fontDialog1.Font;
+                    lb.ForeColor = fontDialog1.Color;
+                }
+                else if(item is Button bt)
+                {
+                    bt.Font = fontDialog1.Font;
+                    bt.ForeColor = fontDialog1.Color;
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
